608588f0237946a81a67d127569df6f8845cde5f
charlie-rasberry <charlie.rasberry@outlook.com>
2026-02-18 22:36:58 +0000

    Preprocessed tagged datasets, fixed CSV formatting issues, and added integrity checks. Also saved mappings for later inference use.

---
+++ src/infer.py
@@ -0,0 +1,4 @@
+# mappings
+binary_map = {'Yes': 1, 'No': 0}
+aspect_map = {'App': 0, 'Driver': 1, 'General': 2, 'Payment': 3, 'Pricing': 4, 'Service': 5}
+sentiment_map = {'Positive': 0, 'Neutral': 1, 'Negative': 2}
